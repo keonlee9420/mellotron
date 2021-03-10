3267e211cad5b8d0cefaeef0bfd93af101563557
KyuminPark <pkm9403@gmail.com>
2021-03-10 13:29:40 +0900

    integrate parallel

--- inference.py
+++ inference.py
@@ -19,6 +19,18 @@ stft = TacotronSTFT(hparams.filter_length, hparams.hop_length, hparams.win_lengt
                     hparams.n_mel_channels, hparams.sampling_rate, hparams.mel_fmin,
                     hparams.mel_fmax)
 
+speaker_id_map = {
+    100000: 0, 100001: 1, 100002: 2, 100003: 3, 100005: 4, 100006: 5, 100007: 6, 100008: 7, 100009: 8, 100011: 9,
+    100012: 10, 100014: 11, 100015: 12, 100016: 13, 100017: 14, 100018: 15, 100019: 16, 100020: 17, 100021: 18,
+    100022: 19, 100023: 20, 100024: 21, 100025: 22, 100026: 23, 100027: 24, 100028: 25, 100029: 26, 100030: 27,
+    100031: 28, 100032: 29, 100033: 30, 100034: 31, 100035: 32, 100036: 33, 100037: 34, 100038: 35, 100039: 36,
+    100041: 37, 100043: 38, 100044: 39, 100045: 40, 100046: 41, 100047: 42, 100048: 43, 100049: 44, 100050: 45,
+    100051: 46, 100054: 47, 100055: 48, 100056: 49, 100057: 50, 100059: 51, 100060: 52, 100061: 53, 100062: 54,
+    100063: 55, 100065: 56, 100066: 57, 100067: 58, 100068: 59, 100069: 60, 100071: 61, 100073: 62, 100074: 63,
+    100075: 64, 100076: 65, 100077: 66, 100079: 67, 100080: 68, 100081: 69, 100082: 70, 100084: 71, 100086: 72,
+    100088: 73, 100089: 74, 100091: 75, 100092: 76, 100093: 77, 100095: 78, 100097: 79, 100099: 80, 100100: 81,
+    100102: 82, 100104: 83, 100105: 84, 100106: 85, 100107: 86, 100108: 87, 100109: 88}
+
 def panner(signal, angle):
     angle = np.radians(angle)
     left = np.sqrt(2) / 2.0 * (np.cos(angle) - np.sin(angle)) * signal
@@ -65,7 +77,7 @@ def vocoder_infer(mel, vocoder, path):
     return wav
 
 
-def inference(dirname, outdir, checkpoint_path):
+def inference(dirname, outdir, checkpoint_path, parallel=False):
     # 멜로트론 로딩
     mellotron = load_model(hparams).cuda().eval()
     mellotron.load_state_dict(torch.load(checkpoint_path)['state_dict'])
@@ -76,20 +88,45 @@ def inference(dirname, outdir, checkpoint_path):
     # 오디오 filelist 로딩
     arpabet_dict = cmudict.CMUDict('data/cmu_dictionary')
     audio_paths = f'data/{dirname}.txt'
-    dataloader = TextMelLoader(audio_paths, hparams)
+    dataloader = TextMelLoader(audio_paths, hparams, speaker_ids=speaker_id_map)
     os.makedirs(f'{outdir}/{os.path.basename(checkpoint_path)}/{dirname}', exist_ok=True)
 
-    with open('data/VCTK/speaker-info.txt') as f:
-        speaker_lines = f.readlines()[1:]
+    with open('data/VCTK/speaker-dict.json') as f:
+        speakers = json.load(f)
 
-    speakers = [l.split()[0].strip() for l in speaker_lines]
     new_filelist = []
+    for file_idx in range(len(dataloader)):
+        audio_path, text, sid = dataloader.audiopaths_and_text[file_idx]
+        print(sid)
+        if not parallel:
+            for sent_txt in sentences:
+                text = sent_txt
 
-    for sent_txt in sentences:
-        for file_idx in range(len(dataloader)):
-            audio_path, text, sid = dataloader.audiopaths_and_text[file_idx]
-            text = sent_txt
+                # get audio path, encoded text, pitch contour and mel for gst
+                text_encoded = torch.LongTensor(text_to_sequence(text, hparams.text_cleaners, arpabet_dict))[None, :].cuda()
+                pitch_contour = dataloader[file_idx][3][None].cuda()
+                mel = load_mel(audio_path)
+                print(audio_path, text)
 
+                # 스피커 id
+                # speaker_name = os.path.basename(audio_path).split('_')[1]
+                # speaker_id = speakers.index(speaker_name)
+                speaker_id = int(sid)
+                speaker_id_mapped = speaker_id_map[speaker_id]
+                speaker_id = torch.LongTensor([speaker_id_mapped]).cuda()
+
+                # 멜로트론 합성
+                with torch.no_grad():
+                    mel_outputs, mel_outputs_postnet, gate_outputs, _ = mellotron.inference(
+                        (text_encoded, mel, speaker_id, pitch_contour))
+
+                    # wav 합성
+                    sample_name = f'{os.path.splitext(os.path.basename(audio_path))[0]}-{text}.wav'
+                    vocoder_infer(mel_outputs_postnet, vocoder,
+                                  f'{outdir}/{os.path.basename(checkpoint_path)}/{dirname}/{sample_name}')
+
+                new_filelist.append(f'{outdir}/{os.path.basename(checkpoint_path)}/{dirname}/{sample_name}\n')
+        else:
             # get audio path, encoded text, pitch contour and mel for gst
             text_encoded = torch.LongTensor(text_to_sequence(text, hparams.text_cleaners, arpabet_dict))[None, :].cuda()
             pitch_contour = dataloader[file_idx][3][None].cuda()
@@ -97,11 +134,11 @@ def inference(dirname, outdir, checkpoint_path):
             print(audio_path, text)
 
             # 스피커 id
-            speaker_name = os.path.basename(audio_path).split('_')[1]
-            speaker_id = speakers.index(speaker_name)
-            sid = int(sid)
-            # print(sid, speaker_id)
-            speaker_id = torch.LongTensor([sid]).cuda()
+            # speaker_name = os.path.basename(audio_path).split('_')[1]
+            # speaker_id = speakers.index(speaker_name)
+            speaker_id = int(sid)
+            speaker_id_mapped = speaker_id_map[speaker_id]
+            speaker_id = torch.LongTensor([speaker_id_mapped]).cuda()
 
             # 멜로트론 합성
             with torch.no_grad():
@@ -118,61 +155,6 @@ def inference(dirname, outdir, checkpoint_path):
     with open(f'{outdir}/{os.path.basename(checkpoint_path)}/{dirname}.txt', 'w') as f:
         f.writelines(new_filelist)
 
-def inference_parallel(dirname, outdir, checkpoint_path):
-    # 멜로트론 로딩
-    mellotron = load_model(hparams).cuda().eval()
-    mellotron.load_state_dict(torch.load(checkpoint_path)['state_dict'])
-
-    # 보코더 로딩
-    vocoder = get_vocoder()
-
-    # 오디오 filelist 로딩
-    arpabet_dict = cmudict.CMUDict('data/cmu_dictionary')
-    audio_paths = f'data/{dirname}.txt'
-    dataloader = TextMelLoader(audio_paths, hparams)
-    datacollate = TextMelCollate(1)
-    os.makedirs(f'{outdir}/{os.path.basename(checkpoint_path)}/{dirname}', exist_ok=True)
-
-    with open('data/VCTK/speaker-info.txt') as f:
-        speaker_lines = f.readlines()[1:]
-
-    speakers = [l.split()[0].strip() for l in speaker_lines]
-    new_filelist = []
-
-    for file_idx in range(len(dataloader)):
-        audio_path, text, sid = dataloader.audiopaths_and_text[file_idx]
-
-        # get audio path, encoded text, pitch contour and mel for gst
-        text_encoded = torch.LongTensor(text_to_sequence(text, hparams.text_cleaners, arpabet_dict))[None, :].cuda()
-        pitch_contour = dataloader[file_idx][3][None].cuda()
-        mel = load_mel(audio_path)
-        print(audio_path, text)
-
-        x, y = mellotron.parse_batch(datacollate([dataloader.get_data((audio_path, text, sid))]))
-
-        # 스피커 id
-        sid = int(sid)
-        speaker_id = torch.LongTensor([sid]).cuda()
-
-        # 멜로트론 합성
-        with torch.no_grad():
-            # get rhythm (alignment map) using tacotron 2
-            mel_outputs, mel_outputs_postnet, gate_outputs, rhythm = mellotron.forward(x)
-            rhythm = rhythm.permute(1, 0, 2)
-
-            mel_outputs, mel_outputs_postnet, gate_outputs, _ = mellotron.inference_noattention(
-                (text_encoded, mel, speaker_id, pitch_contour, rhythm))
-
-            # wav 합성
-            sample_name = f'{os.path.splitext(os.path.basename(audio_path))[0]}-{text}.wav'
-            vocoder_infer(mel_outputs_postnet, vocoder,
-                          f'{outdir}/{os.path.basename(checkpoint_path)}/{dirname}/{sample_name}')
-
-        new_filelist.append(f'{outdir}/{os.path.basename(checkpoint_path)}/{dirname}/{sample_name}\n')
-
-    with open(f'{outdir}/{os.path.basename(checkpoint_path)}/{dirname}.txt', 'w') as f:
-        f.writelines(new_filelist)
-
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser()
@@ -182,7 +164,4 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     for dirname in ['VCTK_val_reference', 'VCTK_val_reference_noisy']:
-        if args.parallel:
-            inference_parallel(dirname, args.outdir, args.checkpoint_path)
-        else:
-            inference(dirname, args.outdir, args.checkpoint_path)
+        inference(dirname, args.outdir, args.checkpoint_path, args.parallel)
